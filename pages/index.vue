e30f1ce176c9b1ddb954e7eb125d365c31bd153a
Joe Winger <git@joewinger.com>
2023-05-22 23:09:00 -0400

    Pull data from database

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,29 @@
 <script setup lang="ts">
-const albums = [
-  {
-    album_name: 'Like Water For Chocolate',
-    spotify_url: 'https://open.spotify.com/album/0ZSwTSaR9VUe3uYsXNQgub',
-    artists: ['Common', 'Artist 2'],
-    album_cover_url: 'https://i.scdn.co/image/ab67616d00001e02e42803c3943b6b0b5481caeb',
-    tags: ['Rap', 'Lyrical'],
-    rating: 4,
-    num_ratings: 14
-  }
-];
+// const albums = [
+//   {
+//     album_name: 'Like Water For Chocolate',
+//     spotify_url: 'https://open.spotify.com/album/0ZSwTSaR9VUe3uYsXNQgub',
+//     artists: ['Common', 'Artist 2'],
+//     album_cover_url: 'https://i.scdn.co/image/ab67616d00001e02e42803c3943b6b0b5481caeb',
+//     tags: ['Rap', 'Lyrical'],
+//     rating: 4,
+//     num_ratings: 14
+//   }
+// ];
+
+let currentPage = 1;
+let resultsPerPage = 50;
+
+const client = useSupabaseClient()
+const { data: albums, error } = await useAsyncData('albums', async () => {
+  const { data } = await client.from('Albums').select('*').range((currentPage-1)*resultsPerPage, currentPage*resultsPerPage);
+  return data
+})
 </script>
 
 <template>
   <header class="bg-blue-500 text-white py-4 px-6">
     <div class="container mx-auto flex items-center justify-between">
-
       <h1 class="text-2xl font-bold">Gym Albums</h1>
       <nav>
         <ul class="flex space-x-4">
