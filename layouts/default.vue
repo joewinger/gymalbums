9fd80137522b7cf84813e2dcd56ce1037d64cf5d
Joe Winger <git@joewinger.com>
2023-05-23 14:32:09 -0400

    Add login/logout buttons

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,15 @@
+<script setup>
+const user = useSupabaseUser()
+const client = useSupabaseAuthClient()
+
+async function login() {
+  const { error } = await client.auth.signInWithOAuth({ provider: 'spotify' })
+  if (error) {
+    return alert('Something went wrong!');
+  }
+}
+</script>
+
 <template>
   <div>
     <header class="bg-blue-500 text-white py-4 px-6">
@@ -5,7 +17,9 @@
         <h1 class="text-2xl font-bold"><a href="/">Gym Albums</a></h1>
         <nav>
           <ul class="flex space-x-4">
-            <li><BaseButton to="/submit">Submit an Album</BaseButton></li>
+            <li><BaseButton to="/submit" :color="user ? 'default' : 'muted'" :disabled="!user">Submit an Album</BaseButton></li>
+            <li v-if="user"><BaseButton @click="client.auth.signOut()">Log Out</BaseButton></li>
+            <li v-else><BaseButton @click="login">Log In</BaseButton></li>
           </ul>
         </nav>
       </div>
